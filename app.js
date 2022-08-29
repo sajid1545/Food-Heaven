a683058d3628f5ef6d37830d3e5683cbff33d89e
sajid9962 <clashking1545@gmail.com>
2022-08-29 19:00:19 +0600

    validation for empty search field done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 document.getElementById('spinner').style.display = 'none';
 
 let loadFoods = (searchText, foodLimit) => {
-	let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`;
+	let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
 	fetch(url)
 		.then((res) => res.json())
-		.then((data) => displayFoods(data.meals));
+		.then((data) => displayFoods(data.meals, foodLimit));
 };
 
 loadFoods('');
@@ -12,8 +12,18 @@ loadFoods('');
 let displayFoods = (meals, foodLimit) => {
 	let foodContainer = document.getElementById('food-container');
 	foodContainer.innerHTML = '';
+
+	// validation for Erro message
+	let errorMessage = document.getElementById('error-message');
+	if (meals.length === 0) {
+		errorMessage.classList.remove('d-none');
+	} else {
+		errorMessage.classList.add('d-none');
+	}
+
 	meals.forEach((meal) => {
 		console.log(meal);
+
 		let foodDiv = document.createElement('div');
 		foodDiv.classList.add('col');
 		foodDiv.innerHTML = `
