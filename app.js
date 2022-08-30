aa9f3d83bbb6676277d83f1c72348c9681c8eabb
sajid9962 <clashking1545@gmail.com>
2022-08-30 08:02:43 +0600

    display food function updated

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ let loadMeals = () => {
 	let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=a`;
 	fetch(url)
 		.then((res) => res.json())
-		.then((data) => console.log(data.meals));
+		.then((data) => displayMeals(data.meals));
 };
+
+let displayMeals = (meals) => {
+	let foodContainer = document.getElementById('food-container');
+	foodContainer.innerHTML = '';
+
+	meals.forEach((meal) => {
+		console.log(meal);
+		let foodDiv = document.createElement('div');
+		foodDiv.classList.add('col');
+		foodDiv.innerHTML = `
+        
+        <div class="card h-100">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0, 100)}
+            </p>
+        </div>
+        </div>
+        `;
+		foodContainer.appendChild(foodDiv);
+	});
+};
+
 loadMeals();
